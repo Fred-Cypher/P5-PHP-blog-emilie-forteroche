bde456725cec6cd022fe68fef99f5307a3e8281a
Fred Perrier <frederique.perrier@gmail.com>
2024-12-10 13:48:06 +0100

    Hide current sorting arrow

--- views/templates/monitoring.php
+++ views/templates/monitoring.php
@@ -3,6 +3,8 @@
 /**
  * Affichage de la page de monitoring
  */
+    $currentSort = $_GET['sort'] ?? null;
+    $currentOrder = $_GET['order'] ?? null;
 ?>
 
 <h2>Monitoring du blog</h2>
@@ -14,15 +16,19 @@
                 <div class="case">
                     <div>Titre de l'article</div>
                     <div class="sortButtons">
-                        <a href="index.php?action=monitoring&sort=article_title&order=asc">
-                            <img src="../../icons/sort1.svg" class="icon" title="Tri croissant" />
-                        </a>
+                        <?php if (!($currentSort === 'article_title' && $currentOrder === 'asc')): ?>
+                            <a href="index.php?action=monitoring&sort=article_title&order=asc">
+                                <img src="../../icons/sort1.svg" class="icon" title="Tri croissant" />
+                            </a>
+                        <?php endif; ?>
                         <a href="index.php?action=monitoring">
                             <img src="../../icons/stop.svg" class="icon" title="Aucun tri" />
                         </a>
-                        <a href="index.php?action=monitoring&sort=article_title&order=desc">
-                            <img src="../../icons/sort2.svg" class="icon" title="Tri décroissant" />
-                        </a>
+                        <?php if (!($currentSort === 'article_title' && $currentOrder === 'desc')): ?>
+                            <a href="index.php?action=monitoring&sort=article_title&order=desc">
+                                <img src="../../icons/sort2.svg" class="icon" title="Tri décroissant" />
+                            </a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </th>
@@ -30,15 +36,19 @@
                 <div class="case">
                     <div>Nombre de vues</div>
                     <div class="sortButtons">
+                        <?php if (!($currentSort === 'article_views' && $currentOrder === 'asc')): ?>
                         <a href="index.php?action=monitoring&sort=article_views&order=asc">
                             <img src="../../icons/sort1.svg" class="icon" title="Tri croissant" />
                         </a>
+                        <?php endif; ?>
                         <a href="index.php?action=monitoring">
                             <img src="../../icons/stop.svg" class="icon" title="Aucun tri" />
                         </a>
+                        <?php if (!($currentSort === 'article_views' && $currentOrder === 'desc')): ?>
                         <a href="index.php?action=monitoring&sort=article_views&order=desc">
                             <img src="../../icons/sort2.svg" class="icon" title="Tri décroissant" />
                         </a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </th>
@@ -46,15 +56,19 @@
                 <div class="case">
                     <div>Nombre de commentaires</div>
                     <div class="sortButtons">
+                        <?php if (!($currentSort === 'comment_count' && $currentOrder === 'asc')): ?>
                         <a href="index.php?action=monitoring&sort=comment_count&order=asc">
                             <img src="../../icons/sort1.svg" class="icon" title="Tri croissant" />
                         </a>
+                        <?php endif; ?>
                         <a href="index.php?action=monitoring">
                             <img src="../../icons/stop.svg" class="icon" title="Aucun tri" />
                         </a>
+                        <?php if (!($currentSort === 'comment_count' && $currentOrder === 'desc')): ?>
                         <a href="index.php?action=monitoring&sort=comment_count&order=desc">
                             <img src="../../icons/sort2.svg" class="icon" title="Tri décroissant" />
                         </a>
+                        <?php endif; ?>
                     </div>
                 </div>
 
@@ -63,15 +77,19 @@
                 <div class="case">
                     <div>Date de publication</div>
                     <div class="sortButtons">
+                        <?php if (!($currentSort === 'article_date_creation' && $currentOrder === 'asc')): ?>
                         <a href="index.php?action=monitoring&sort=article_date_creation&order=asc">
                             <img src="../../icons/sort1.svg" class="icon" title="Tri croissant" />
                         </a>
+                        <?php endif; ?>
                         <a href="index.php?action=monitoring">
                             <img src="../../icons/stop.svg" class="icon" title="Aucun tri" />
                         </a>
+                        <?php if (!($currentSort === 'article_date_creation' && $currentOrder === 'desc')): ?>
                         <a href="index.php?action=monitoring&sort=article_date_creation&order=desc">
                             <img src="../../icons/sort2.svg" class="icon" title="Tri décroissant" />
                         </a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </th>
